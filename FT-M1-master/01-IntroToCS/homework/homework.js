8198e9319feee8ce97ae488acc775c63c4dc1f15
Denisse Gutierrez <fgutierrezc60@gmail.com>
2022-07-05 09:54:55 -0500

    Homework I

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,15 +2,38 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let valor = 0;
+  let potencia= num.length-1;
 
+  for(let i=0; i<num.length; i++){
+    valor=valor + Number(num[i]) * Math.pow(2,potencia);
+    potencia--;
+  }
+  return valor;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let result = [];
+  let repite = true;
+  let residuo;
+  let cociente;
 
+  while(repite){
+    residuo = num % 2;
+    cociente = (num-residuo)/2;
+
+    result.push(residuo);
+    num= cociente;
+
+    if(num===1){
+      result.push(num);
+      repite = false;
+    }
+  }
+  return result.reverse().join("");
 }
-
-
+    
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
